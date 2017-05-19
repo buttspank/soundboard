1ace565b359b199425bfbef1ba527965b0be8ffe
TAZell <thomas77843@students.sbcsc.k12.in.us>
2017-05-19 09:01:51 -0400

    Stuff
    Chqanges I made

--- SoundBoard/app/src/main/java/org/pltw/examples/soundboard/Sith.java
+++ SoundBoard/app/src/main/java/org/pltw/examples/soundboard/Sith.java
@@ -1,7 +1,26 @@
 package org.pltw.examples.soundboard;
 
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.Button;
+
 /**
  * Created by anthony90113 on 5/18/2017.
  */
 public class Sith extends MainActivity {
+    private final int WHOLE_NOTE = 1000;
+    public static final String TAG = Sith.class.getName();
+    public Button mVader;
+    //public Button mFbutton;
+    private MediaPlayer mDV1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sith);
+
+        mVader = (Button) findViewById(R.id.vader);
+        //mFbutton = (Button) findViewById(R.id.fButton);
+        mDV1 = MediaPlayer.create(this, R.raw.darthvader_yourfather);
+    }
 }
